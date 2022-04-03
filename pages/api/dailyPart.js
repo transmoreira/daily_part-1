bb5fda15f30f22b97fab15dc1ab398a8dc9393a3
DIVANIR <divanirjs@gmail.com>
2022-04-03 00:19:49 -0300

    Configurando Headers api

--- pages/api/dailyPart.js
+++ pages/api/dailyPart.js
@@ -3,7 +3,6 @@ import connect from "./connect"
 const dailyPart = (request, response) => {
 
     request.setHeader("access-control-allow-origin","https://dahoracartaodeponto.com/")
-    request.setHeader("Access-Control-Allow-Methods",["GET", "POST", "PUT"])
     
     if (request.method === "GET") {
         const {start, end} = request.query
