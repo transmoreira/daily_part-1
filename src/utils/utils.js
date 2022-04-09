fc9fb01fab401696b62174ca8ddbb5e5a88c4963
Divanir de Jesus Silva <50552000+DIVANIR@users.noreply.github.com>
2022-04-09 06:17:03 -0300

    Update utils.js
    Desfazendo correção da data

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 const dateFormated = (date, brazil=true) => {
     
-    const day = (date.getDate()+1).toString().padStart(2,"0")
+    const day = (date.getDate()).toString().padStart(2,"0")
     const month = (date.getMonth() + 1).toString().padStart(2,"0")
     const year = date.getFullYear()    
 
