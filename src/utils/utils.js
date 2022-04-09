8d3881d842e5175b9b94e2c26819ea1b03a7670e
DIVANIR <divanirjs@gmail.com>
2022-04-09 17:36:46 -0300

    Corrigindo UTC

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 const dateFormated = (date, brazil=true) => {
-   
+    date.setUTCHours(3)
     const day = (date.getDate()).toString().padStart(2,"0")
     const month = (date.getMonth() + 1).toString().padStart(2,"0")
     const year = date.getFullYear()    
