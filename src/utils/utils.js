53237fe7d16fb09b771aa2a3a4c5cf7f5c1d3328
Divanir de Jesus Silva <50552000+DIVANIR@users.noreply.github.com>
2022-04-22 12:19:28 -0300

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,15 @@
-
-
 const dateFormated = (date, brazil=true) => {
     date.setUTCHours(3)
     const day = (date.getDate()).toString().padStart(2,"0")
     const month = (date.getMonth() + 1).toString().padStart(2,"0")
     const year = date.getFullYear()    
     
-    return brazil ? `${day}/${month}/${year}` : `${year}-${month}-${day}`
+    return brazil ? date.toLocaleDateString() : `${year}-${month}-${day}`
 }
 
-const timeFormated = (time) => {
-   
+const timeFormated = (time, ) => {
+    
     try{
-        
         const timeString = time.toString().substr(11,5)
         return timeString != "00:00" ? timeString : ""
     }catch(erro){
@@ -28,5 +25,14 @@ const filter = (dailyPart, filterDatas) =>  dailyPart.filter((item, index) => {
             (filterDatas.line == "" || item.travels.map(item => item.line).filter(item => item == filterDatas.line).length > 0)
     })
 
+const unproductiveKm = ()=>({
+    name:"DESLOCAMENTO OCIOSO",
+    direction:[
+        "INICIO DE ROTA",
+        "FIM DE ROTA",
+        "CASA",
+        'GARAGEM'
+    ]
+})
 
-export {dateFormated, timeFormated, filter}
+export {dateFormated, timeFormated, filter, unproductiveKm}
