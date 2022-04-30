c221b157702c9b5ff1876b965226041674006fe4
Divanir de Jesus Silva <50552000+DIVANIR@users.noreply.github.com>
2022-04-30 10:29:31 -0300

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -31,13 +31,13 @@ const filter = (dailyPart, filterDatas) =>  dailyPart.filter((item, index) => {
 const exportCsv = (data)=>{
     
     
-    const csvContent = "DATA;LINHA;ORIGEM;DESTINO;HOAR INICIO;HORA FIM;PASSAGEIRO;KM INICIO;KM FIM; TOTAL KM; CARRO;MOTORISTA\n"
+    const csvContent = "DATA;LINHA;ORIGEM;DESTINO;HOAR INICIO;HORA FIM;PASSAGEIRO;KM INICIO;KM FIM; TOTAL KM; CARRO;MATRICULA;MOTORISTA\n"
         + data.map(dailyPart=>{
         return dailyPart.travels.filter(travel=>travel.line!="DESLOCAMENTO OCIOSO").map(travel=>{
             const date = travel.startTime.toString().substr(0,10)
             const startTime = travel.startTime.toString().substr(12,5)
             const endTime = travel.endTime.toString().substr(11,5)
-            return `${date};${travel.line};${travel.origin};${travel.destiny};${startTime};${endTime};${travel.passenger};${travel.startKM};${travel.endKM};${travel.endKM-travel.startKM};${dailyPart.car.number};${dailyPart.driver.name}\n`
+            return `${date};${travel.line};${travel.origin};${travel.destiny};${startTime};${endTime};${travel.passenger};${travel.startKM};${travel.endKM};${travel.endKM-travel.startKM};${dailyPart.car.number};${dailyPart.driver.registration};${dailyPart.driver.name}\n`
         }).join("")
     }).join("")
     
