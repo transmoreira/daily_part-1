e0f98dce9a425a68719c90001625ef5eed754822
Divanir de Jesus Silva <50552000+DIVANIR@users.noreply.github.com>
2022-05-11 10:28:43 -0300

    Correct Travels

--- pages/api/travels.js
+++ pages/api/travels.js
@@ -2,8 +2,6 @@ import connect from "./connect"
 
 const dailyPart = async (request, response) => {
 
-    const recursive = ()=>{
-
     const getResult = async (sql) => {
         try {            
 
@@ -16,64 +14,56 @@ const dailyPart = async (request, response) => {
             //console.log(error)
             response.status(400).json(error)
             //getResult(sql)
-            recursive()
         }
     }
 
-    
-        if (request.method === "POST") {
-            const { id_daily_part, line, startTime, startKM, origin, destiny, direction, startTicket = 0 } = JSON.parse(request.body)
-            const sql = `INSERT INTO 
-                            travels( 
-                                id_daily_part, 
-                                line, 
-                                startTime,
-                                startKM,
-                                origin,
-                                destiny,
-                                direction,
-                                startTicket
-    
-                            ) 
-                            VALUES (
-                                '${id_daily_part}', 
-                                '${line}', 
-                                '${startTime}',
-                                '${startKM}',
-                                '${origin}',
-                                '${destiny}',
-                                '${direction}',
-                                '${startTicket}'
-                            )`
+    if (request.method === "POST") {
+        const { id_daily_part, line, startTime, startKM, origin, destiny, direction, startTicket = 0 } = JSON.parse(request.body)
+        const sql = `INSERT INTO 
+                        travels( 
+                            id_daily_part, 
+                            line, 
+                            startTime,
+                            startKM,
+                            origin,
+                            destiny,
+                            direction,
+                            startTicket
+
+                        ) 
+                        VALUES (
+                            '${id_daily_part}', 
+                            '${line}', 
+                            '${startTime}',
+                            '${startKM}',
+                            '${origin}',
+                            '${destiny}',
+                            '${direction}',
+                            '${startTicket}'
+                        )`
+        await getResult(sql)
+
+
+    } else if (request.method === "PUT") {
+        const { id, endTime, endKM, passenger, endTicket = 0 } = JSON.parse(request.body)
+     
+        const sql = `UPDATE travels  
+                        SET endTime='${endTime}',
+                        endKM=${parseInt(endKM)},
+                        passenger=${parseInt(passenger)},
+                        endTicket=${parseInt(endTicket)}
+                    WHERE id = ${id}`
+
+        try{
             await getResult(sql)
-    
-    
-        } else if (request.method === "PUT") {
-            const { id, endTime, endKM, passenger, endTicket = 0 } = JSON.parse(request.body)
-         
-            const sql = `UPDATE travels  
-                            SET endTime='${endTime}',
-                            endKM=${parseInt(endKM)},
-                            passenger=${parseInt(passenger)},
-                            endTicket=${parseInt(endTicket)}
-                        WHERE id = ${id}`
-    
-            try{
-                await getResult(sql)
-            }catch(erro){
-                response.json(error)
-                recursive()
-            }
-        }else if(request.method === "DELETE"){
-            const { id } = JSON.parse(request.body)
-            const sql = "DELETE FROM travels WHERE id  = " + id
-            getResult(sql)
-            recursive()
+        }catch(erro){
+            response.json(error)
         }
+    }else if(request.method === "DELETE"){
+        const { id } = JSON.parse(request.body)
+        const sql = "DELETE FROM travels WHERE id  = " + id
+        getResult(sql)
     }
-
-    recursive()
-    
 }
 
 
